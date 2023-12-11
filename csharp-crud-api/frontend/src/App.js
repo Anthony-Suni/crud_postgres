b30ede6ecb9ac9e951f81f0cc4fab66630f8a6f8
Anthony-Suni <jonathan.suni@tecsup.edu.pe>
2023-12-11 10:06:02 -0500

    Mensaje descriptivo de tus cambios

--- csharp-crud-api/frontend/src/App.js
+++ csharp-crud-api/frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Cambiado de Switch a Routes
+import UserList from './components/UserList';
+import UserForm from './components/UserForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>  {/* Cambiado de Switch a Routes */}
+        <Route path="/users" element={<UserList />} />
+        <Route path="/users/add" element={<UserForm />} />
+        <Route path="/users/edit/:id" element={<UserForm />} />
+      </Routes>  {/* Cambiado de Switch a Routes */}
+    </Router>
   );
 }
 
