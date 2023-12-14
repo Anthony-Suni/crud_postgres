a98d5955bb3a9a23c0cf8567a83e405110c1a5b9
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-14 12:34:17 -0500

    Update test.sql

--- csharp-crud-api/test.sql
+++ csharp-crud-api/test.sql
@@ -1,5 +1,14 @@
+-- Crear la base de datos
+CREATE DATABASE Recomendaciones;
+
+-- Conectarse a la base de datos Recomendaciones
+\c Recomendaciones;
+
+-- Crear la tabla users
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL
 );
+
+-- Puedes agregar más comandos SQL según sea necesario para tu aplicación.
