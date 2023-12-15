00ca542d1897195547fd11c604b2217db48e093b
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-14 19:23:08 -0500

    Update User.cs

--- csharp-crud-api/Models/User.cs
+++ csharp-crud-api/Models/User.cs
@@ -2,16 +2,25 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models
 {
-  [Table("users")]
-  public class User
-  {
-    [Column("id")]
-    public int Id { get; set; }
+    [Table("users")]
+    public class User
+    {
+        [Column("id")]
+        public int Id { get; set; }
 
-    [Column("name")]
-    public string Name { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
 
-    [Column("email")]
-    public string Email { get; set; }
-  }
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("genero")] // Agregado: Asegúrate de que coincida con el nombre de la columna en tu base de datos
+        public string Genero { get; set; }
+
+        [Column("pelicula")] // Agregado: Asegúrate de que coincida con el nombre de la columna en tu base de datos
+        public string Pelicula { get; set; }
+
+        [Column("rating")] // Agregado: Asegúrate de que coincida con el nombre de la columna en tu base de datos
+        public int Rating { get; set; }
+    }
 }
