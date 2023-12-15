829db87ea7f03984e434d1489f674b85ab14f4bd
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-14 19:24:40 -0500

    Update test.sql

--- csharp-crud-api/test.sql
+++ csharp-crud-api/test.sql
@@ -8,7 +8,11 @@ CREATE DATABASE recomendaciones;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
+    email VARCHAR(255) NOT NULL,
+    genero VARCHAR(255), -- Asegúrate de que coincida con el nombre de la propiedad en tu modelo
+    pelicula VARCHAR(255), -- Asegúrate de que coincida con el nombre de la propiedad en tu modelo
+    rating INT -- Asegúrate de que coincida con el nombre de la propiedad en tu modelo
 );
+
 
 -- Puedes agregar más comandos SQL según sea necesario para tu aplicación.
